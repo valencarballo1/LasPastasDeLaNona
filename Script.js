6fc8e5695da85763c698a4b0586bc2332c2d248f
Valentin Carballo <valentincarballo99@gmail.com>
2024-09-02 14:39:05 -0300

    arreglo titulo modal

--- Script.js
+++ Script.js
@@ -1,28 +1,28 @@
-$(document).ready(function() {
-    // Escucha el evento de clic en las "cards"
-    
+$(document).ready(function () {
+  // Escucha el evento de clic en las "cards"
 
-  });
 
-  $('.generoModal').click(function() {
-    let comida = $(this).attr('data-comida'); // Obtiene el tipo de comida desde el atributo data-comida
-    // $('#genericModalTitle').text(comida); // Actualiza el título del modal con el tipo de comida seleccionado
-    llenarModal(comida); // Llena el contenido del modal según el tipo de comida seleccionado
+});
+
+$('.generoModal').click(function () {
+  let comida = $(this).attr('data-comida'); // Obtiene el tipo de comida desde el atributo data-comida
+  // $('#genericModalTitle').text(comida); // Actualiza el título del modal con el tipo de comida seleccionado
+  llenarModal(comida); // Llena el contenido del modal según el tipo de comida seleccionado
 });
 
 function llenarModal(comida) {
   obtenerOpciones(comida)
     .then(opciones => {
-      var modalBody = $('#genericModalBody');
+      const modalBody = $('#genericModalBody');
       modalBody.empty();
 
       console.log("Entra en las opciones:", opciones);
 
       // Llena la tabla con las opciones específicas de la comida
-      for (var i = 0; i < opciones.length; i++) {
-          var opcion = opciones[i];
-          var row = '<tr><td>' + opcion.nombre + '</td><td>' + opcion.descripcion + '</td><td>' + '$' + opcion.precio + '</td></tr>';
-          modalBody.append(row);
+      for (let i = 0; i < opciones.length; i++) {
+        const opcion = opciones[i];
+        const row = '<tr><td>' + opcion.nombre + '</td><td>' + opcion.descripcion + '</td><td>' + '$' + opcion.precio + '</td></tr>';
+        modalBody.append(row);
       }
     })
     .catch(error => console.error('Error obteniendo las opciones:', error));
@@ -30,7 +30,7 @@ function llenarModal(comida) {
 
 function obtenerOpciones(comida) {
   const url = 'https://valencarballo1.github.io/LasPastasDeLaNona/menu.json'; // La URL es relativa a la ubicación de tu archivo HTML
-  
+
   return fetch(url)  // Se agrega el return aquí
     .then(response => {
       if (!response.ok) {
@@ -40,6 +40,11 @@ function obtenerOpciones(comida) {
     })
     .then(menu => {
       if (menu.hasOwnProperty(comida)) {
+        const modalTitle = $("#genericModalTitle");
+        modalTitle.empty();
+
+        let titulo = '<h5 class="text-center">' + comida + '</h5>';
+        modalTitle.append(titulo);
         return menu[comida];  // Se retorna el resultado deseado
       } else {
         throw new Error('Tipo de comida no encontrado');
